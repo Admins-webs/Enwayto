e89e0ff7700af0a3035240300be66aa2253b691c
Admins-webs <astaslasx@gmail.com>
2025-12-03 16:51:14 +0700

    Update script.js

--- page/docs/script.js
+++ page/docs/script.js
@@ -703,11 +703,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (!settings || Object.keys(settings).length === 0) return
 
     const currentYear = new Date().getFullYear()
-    const creator = settings.apiSettings?.creator || "Raol Api'S"
+    const creator = settings.apiSettings?.creator || "DeepApi'S"
 
-    setPageContent(DOM.pageTitle, settings.name, "Raol Api'S")
-    setPageContent(DOM.wm, `© ${currentYear} Raol Api'S Corp. All rights reversed.`)
-    setPageContent(DOM.appName, settings.name, "Raol Api'S")
+    setPageContent(DOM.pageTitle, settings.name, "DeepApi'S")
+    setPageContent(DOM.wm, `© ${currentYear} DeepApi'S Corp. All rights reversed.`)
+    setPageContent(DOM.appName, settings.name, "DeepApi'S")
     setPageContent(DOM.sideNavName, settings.name || "API")
     setPageContent(DOM.versionBadge, settings.version, "v1.0")
     setPageContent(DOM.versionHeaderBadge, settings.header?.status, "Active!")
